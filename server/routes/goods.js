c059856b8f94da51c7b1ea4b15f71f60e50ebe8b
JakeVivit <695048027@qq.com>
2018-05-19 18:22:56 +0800

    vue-node-dome

---
+++ server/routes/goods.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Goods = require('../models/good.js');
+
+//连接数据库；
+mongoose.connect('mongodb://my_root:zhang4601592@47.92.73.203:27017/admin');
+
+//监听数据库连接
+
+mongoose.connection.on("connected",function () {
+  console.log('MongoDB connected success');
+})
+
+mongoose.connection.on("erroe",function () {
+  console.log('MongoDB connected fail');
+})
+
+mongoose.connection.on("disconnected",function () {
+  console.log('MongoDB connected disconnected');
+})
+
+//实现路由
+
+router.get('/', function (req, res, next) {
+  res.send('goods is success');
+})
+
+//输出
+module.exports = router;
---
+++ src/views/goodsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="goodsList">
+    <herder></herder>
+    <h1>list页面</h1>
+  </div>
+</template>
+
+<script>
+  import herder from '../components/header.vue'
+  export default {
+    data() {
+      return{}
+    },
+    components:{
+      herder
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
